a5f61b0c1fd8926b1c9c82183303f493e5365fcf
sakshis1512 <shiradkarsakshi@gmail.com>
2025-08-20 16:24:41 +0530

    Initial commit

---
+++ pdf-invoice-app/src/main/java/com/invoice/main/PdfInvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.invoice.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfInvoiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfInvoiceAppApplication.class, args);
+	}
+
+}
